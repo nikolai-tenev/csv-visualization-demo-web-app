a8e9298183e822051c51aa5b6366c2cb7ee180b8
bh2o <tenev.bh2o@gmail.com>
2020-03-31 18:46:11 +0300

    1. Update .gitignore. 2. Setup CRA. 3. Setup MUI.

---
+++ src/Application.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {CssBaseline} from "@material-ui/core";
+
+class Application extends Component {
+    render() {
+        return <>
+            <CssBaseline/>
+        </>;
+    }
+}
+
+export default Application;
