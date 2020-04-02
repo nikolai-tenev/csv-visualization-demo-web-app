b2415d6f98fb1ad149159e8c1b7d3efa22d67e43
bh2o <tenev.bh2o@gmail.com>
2020-04-02 12:29:56 +0300

    1. Add a default page.

--- src/configs/application-urls.js
+++ src/configs/application-urls.js
@@ -1,3 +1,5 @@
+export const HOMEPAGE_URL = "/";
+
 export const LOGIN_PAGE_URL = "/login";
 export const REGISTER_PAGE_URL = "/register";
 
@@ -5,3 +7,5 @@ export const DASHBOARD_PAGE_URL = "/dashboard";
 
 export const DATASETS_PAGE_URL = "/datasets";
 export const DATASETS_EDIT_PAGE_URL = "/datasets/:id";
+
+export const DEFAULT_PAGE_URL = DASHBOARD_PAGE_URL;
